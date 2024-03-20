2452f8e5ca4b8933dd094492d159fc83538f5bc6
PaoloMarzocchi <marzocchi.3p@gmail.com>
2024-03-20 15:55:53 +0100

    add title in DOM

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,21 @@
 <script>
-
+export default{
+  data(){
+    return{
+      title: 'Hello vite! 🏃‍♂️'
+    }
+  }
+}
 
 </script>
 
 <template>
-  
+  <h1>{{ title }}</h1>
 </template>
 
 <style>
-
+h1{
+  text-align: center;
+  margin: 2rem 0;
+}
 </style>
